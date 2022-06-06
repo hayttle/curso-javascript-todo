8589883692b281b384b0041809789a71ddb5f85a
hayttle <hayttle@gmail.com>
2022-06-05 21:36:57 -0300

    refactored after analysis

--- app.js
+++ app.js
@@ -2,103 +2,75 @@ const inputSearchTodo = document.querySelector(".search input");
 const inputAddTodo = document.querySelector(".inputAddTodo");
 const todosContainer = document.querySelector("ul");
 const form = document.querySelector("form");
-const btnAddTodo = document.querySelector(".btnAddTodo");
 
-const addTodo = (inputAddTodo) => {
-  if (inputAddTodo) {
-    todosContainer.insertAdjacentHTML(
-      "afterbegin",
-      `
-      <li><span>
-            ${inputAddTodo}
+const addTodo = (event) => {
+  event.preventDefault();
+  const todo = inputAddTodo.value.trim();
+  event.target.reset();
+
+  if (!todo) {
+    return;
+  }
+  todosContainer.insertAdjacentHTML(
+    "afterbegin",
+    `
+      <li data-todo="${todo}"><span>
+            ${todo}
           </span>
           <span>
-            <i class="fas fa-check-circle"></i>
-            <i class="fas fa-trash"></i>
+          <i class="fas fa-check-circle" data-check="${todo}"></i>
+          <i class="fas fa-trash" data-trash="${todo}"></i>
           </span>
-      </li>
-      `
-    );
+          </li>`
+  );
+};
+
+const reorganizeTodos = (checkWasClicked) => {
+  const todo = document.querySelector(`[data-todo="${checkWasClicked}"]`);
+  const todoWasMarkedAsDone = todo.classList.contains("done");
+
+  todo.classList.toggle("done");
+
+  if (todoWasMarkedAsDone) {
+    todosContainer.prepend(todo);
+    return;
   }
+
+  todosContainer.append(todo);
 };
 
-const removeTodo = (li) => {
-  li.remove();
-};
-
-const doneTodo = (li) => {
-  li.classList.toggle("done");
-};
-
-const insertTodoIntoDOM = (todos) => {
-  let HTMLTemplate = "";
-  todos.forEach((todo) => {
-    HTMLTemplate += `
-          <li class="${todo.className}">
-           <span>${todo.innerText}</span>
-            <span>
-              <i class="fas fa-check-circle"></i>
-              <i class="fas fa-trash"></i>
-            </span>
-          </li>`;
-  });
-  todosContainer.innerHTML = HTMLTemplate;
-};
-
-const changePositionTodoToEnd = (li, todos) => {
-  doneTodo(li);
-  todos.push(todos.splice(todos.indexOf(li), 1)[0]);
-  insertTodoIntoDOM(todos);
-};
-
-const changePositionTodoToBegin = (li, todos) => {
-  doneTodo(li);
-  todos.unshift(todos.splice(todos.indexOf(li), 1)[0]);
-  insertTodoIntoDOM(todos);
-};
-
-const hideTodo = (searchTodo, todos) => {
-  todos
-    .filter((todo) => !todo.textContent.toLowerCase().includes(searchTodo))
-    .forEach((todo) => {
-      todo.classList.add("hidden");
-    });
-};
-
-const showTodo = (searchTodo, todos) => {
-  todos
-    .filter((todo) => todo.textContent.toLowerCase().includes(searchTodo))
-    .forEach((todo) => {
-      todo.classList.remove("hidden");
-    });
-};
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  addTodo(inputAddTodo.value.trim());
-  event.target.reset();
-});
-
-inputSearchTodo.addEventListener("input", (event) => {
-  const searchTodo = event.target.value.trim().toLowerCase();
-  const todos = Array.from(todosContainer.children);
-
-  hideTodo(searchTodo, todos);
-  showTodo(searchTodo, todos);
-});
-
-todosContainer.addEventListener("click", (event) => {
+const handleTodoClick = (event) => {
   const clickedElement = event.target;
-  const li = clickedElement.parentElement.parentElement;
-  const classesTodo = Array.from(clickedElement.classList);
+  const trashWasClicked = clickedElement.dataset.trash;
+  const checkWasClicked = clickedElement.dataset.check;
 
-  if (classesTodo.includes("fa-trash")) {
-    removeTodo(li);
-    return
+  if (trashWasClicked) {
+    const todo = document.querySelector(`[data-todo="${trashWasClicked}"]`);
+    todo.remove();
+    return;
   }
-  
-  if (classesTodo.includes("fa-check-circle")) {
-    const todos = Array.from(todosContainer.children);
-    li.className !== "done" ? changePositionTodoToEnd(li, todos) : changePositionTodoToBegin(li, todos);
+
+  if (checkWasClicked) {
+    reorganizeTodos(checkWasClicked);
   }
-});
+};
+
+const searchTodo = (event) => {
+  const inputValue = event.target.value.trim().toLowerCase();
+  const todos = Array.from(todosContainer.children).map((todo) => ({
+    todo,
+    shouldBeVisible: todo.textContent.toLowerCase().includes(inputValue),
+  }));
+
+  todos.forEach(({todo,shouldBeVisible}) => {
+    if(!shouldBeVisible){
+      todo.classList.add('hidden')
+      return
+    }
+    todo.classList.remove('hidden')
+  });
+};
+
+form.addEventListener("submit", addTodo);
+todosContainer.addEventListener("click", handleTodoClick);
+inputSearchTodo.addEventListener("input", searchTodo);
